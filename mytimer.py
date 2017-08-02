d6cd549d2f155387f79b12775deda0e60a1fdb48
magndahl <magda@aarhus.dk>
2017-08-02 16:16:46 +0200

    Little timer class

---
+++ mytimer.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 02 16:06:12 2017
+
+@author: azfv1n8
+"""
+
+import time
+
+class Timer(object):
+    def __init__(self, name=None):
+        self.name = name
+
+    def __enter__(self):
+        self.tstart = time.time()
+
+    def __exit__(self, type, value, traceback):
+        if self.name:
+            print '[%s]' % self.name,
+        print 'Elapsed: %s' % (time.time() - self.tstart)
