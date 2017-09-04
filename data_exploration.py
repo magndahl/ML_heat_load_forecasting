c4b6ddd85e1d489db44c537e1a22ed34037d4f0a
magndahl <magda@aarhus.dk>
2017-09-04 12:55:57 +0200

    Explore lagged data as well

--- data_exploration.py
+++ data_exploration.py
@@ -12,6 +12,7 @@ import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 import pipeline as pl
 from build_data_set import categorical_columns
+from scipy.stats import boxcox
 
 
 
@@ -30,8 +31,12 @@ scaler = pl.StandardScalerIgnoreDummies(dummy_column_ix, StandardScaler())
 
 arr = arr_dict['array']
 
-arr_scaled = scaler.transform(arr)
+arr_scaled = scaler.fit_transform(arr)
 
+    
+y = arr_scaled[:,0]
+X = arr_scaled[:,1:]
+    
 
 def all_joint_plots(arr_scaled, variables=arr_dict['headers']):
     
@@ -45,6 +50,24 @@ def all_joint_plots(arr_scaled, variables=arr_dict['headers']):
         plt.close()
     
     return
+
+
+def joint_plots_lagged_weather(arr_scaled, variables=arr_dict['headers']):
+    scaled_df = pd.DataFrame(data=arr_scaled, columns=variables)
+    figpath = 'figures/data_exploration/prod_vs_lagged_weather/'
+    weather_vars = ['Tout', 'vWind', 'sunRad']
+    lags = np.arange(1,24)
+    
+    for w_var in weather_vars:
+        for lag in lags:
+            col_name = w_var + 'lag%i'%lag
+            scaled_df[col_name] = scaled_df[w_var].shift(lag)
+            
+            filename = 'prod_vs_' + col_name + '.png'
+            sns.jointplot(x=col_name, y='prod', data=scaled_df, alpha=0.1)
+            plt.savefig(figpath + filename)
+            plt.close()
+    
 
 
 def corr_coeff_plot(arr_scaled, variables=arr_dict['headers']):
